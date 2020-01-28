6d004e246468fb01c063fe7a2c0cfe6100513e17
어유선 <shinecraft@naver.com>
2020-01-28 14:16:08 +0000

    20200128 test run for zetta and nodered done

---
+++ Practice/zettaPractice.js
@@ -0,0 +1,8 @@
+process.EventEmitter = require('events').EventEmitter; // add this line
+var zetta = require('zetta');
+
+zetta()
+  .name('Server Name')
+  .listen(8888, function(){
+     console.log('Zetta is running at http://127.0.0.1:1337');
+});
