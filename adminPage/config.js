1787e3af6c07185f22b09059b4dd5cd8df62df7a
2015004711 <shinecraft@naver.com>
2020-05-03 22:30:39 +0900

    get fiware complete

---
+++ adminPage/config.js
@@ -0,0 +1,6 @@
+var orionCB = {
+    host : "localhost",
+    port : "1026"
+}
+
+module.exports.orionCB = orionCB;
--- adminPage/index.js
+++ adminPage/index.js
@@ -1,17 +1,29 @@
 const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require('http');
+const httpserver = http.createServer(app);
+const io = require('socket.io')(httpserver);
+var config = require('./config.js')
 
-const host = '127.0.0.1'
-const port = 7777;
 
 app.get('/',function(req,res){
     res.status(200).send("OK : 200");
 });
 
 app.get('/main',function(req,res){
+    var fiwareConfig = config.orionCB;
+    fiwareConfig.path = '/version'
+    var fiwareData;
+    http.request(fiwareConfig, function(response){
+        console.log(fiwareConfig)
+        getFromCB(response,function(fiwareData){
+            console.log("fiware data\n"+fiwareData)
+        });
+        
+    }).end();
+    
     res.sendFile(__dirname + '/adminMain.html')
 })
+
 
 io.on('connection', function(socket){
     console.log('socket on!')
@@ -23,6 +35,20 @@ io.on('connection', function(socket){
 })
 
 
-http.listen(port,function(){
-    console.log(host+':'+port.toString())
+httpserver.listen(7777,function(){
+    console.log('localhost:7777')
 })
+
+function getFromCB(response,callback) {
+    var CBdata = '';
+    response.on('data', function (chunk) {
+      CBdata += chunk;
+    });
+    response.on('end', function () {
+      console.log("received server data:");
+      console.log(CBdata);
+      callback(CBdata)
+    });
+  }
+
+
