9bbb87045c3f79acdd88cc3e3b91f4e7190b6f62
JunHyeok Bae <bjh5098@naver.com>
2020-09-19 00:31:42 +0900

    sio IO

--- raspberry/SensorControl/PIR_Sensor_LED_ON.py
+++ raspberry/SensorControl/PIR_Sensor_LED_ON.py
@@ -1,5 +1,6 @@
-import spidev, time
+import spidev, time, socketio, json
 import RPi.GPIO as GPIO
+from collections import OrderedDict
 
 led = 18
 pir = 25
@@ -7,16 +8,41 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(pir,GPIO.IN)
 GPIO.setup(led,GPIO.OUT)
 
+sio = socketio.Client()
+
+json_sensordata = OrderedDict()
+
 try :
     while True:
         if GPIO.input(pir) == True:
             print("Sensor ON")
             GPIO.output(led, GPIO.HIGH)
             time.sleep(0.2)
+            
         if GPIO.input(pir) == False:
             print("Sensor OFF")
             GPIO.output(led, GPIO.LOW)
             time.sleep(0.2)
+            
 except KeyboardInterrupt:
     GPIO.cleanup()
 
+@sio.on('connect')
+def handler():
+    print("socket connected")
+
+@sio.on('connection_error')
+def handler():
+    print("connection fail")
+
+@sio.on('order')
+def hadler(data):
+    #if(data.value = "some order"){}
+    #elif(data.value = "other order"){}
+    print("recieved order")
+    json_sensordata["value"] = GPIO.input(pir)
+    sio.emit('order', json_sensordata)      
+    print("send data")
+    sio.emit('disconnect')
+
+sio.connect('http://localhost:3000')
