34cbae328b8da15471bb98b761812d9fbf40b9b5
JunHyeok Bae <bjh5098@naver.com>
2020-10-13 18:08:55 +0900

    temp read

--- raspberry/SensorControl/Temp_Sensor_datasend.py
+++ raspberry/SensorControl/Temp_Sensor_datasend.py
@@ -1,33 +1,23 @@
+import time
+import Adafruit_DHT
 import spidev, time, socketio, json
 import RPi.GPIO as GPIO
+import threading
 from collections import OrderedDict
 
+sensor = Adafruit_DHT.DHT11
+
 led = 18
-pir = 25
+temp = 4
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pir,GPIO.IN)
+#GPIO.setup(pir,GPIO.IN)
 GPIO.setup(led,GPIO.OUT)
 
 sio = socketio.Client()
-
 json_sensordata = OrderedDict()
 
-"""
-try :
-    while True:
-        if GPIO.input(pir) == True:
-            print("Sensor ON")
-            GPIO.output(led, GPIO.HIGH)
-            time.sleep(0.2)
-            
-        if GPIO.input(pir) == False:
-            print("Sensor OFF")
-            GPIO.output(led, GPIO.LOW)
-            time.sleep(0.2)
-            
-except KeyboardInterrupt:
-    GPIO.cleanup()
-"""
+#set_interval(ledblink,0.05) #0.2초 간격으로 Led Blink 함수 실행
 
 @sio.on('connect')
 def handler():
@@ -38,13 +28,20 @@ def handler():
     print("connection fail")
 
 @sio.on('order')
-def hadler(data):
+def handler(data):
     #if(data.value = "some order"){}
     #elif(data.value = "other order"){}
     print("recieved order")
-    json_sensordata["value"] = GPIO.input(pir)
+    
+    h, t = Adafruit_DHT.read_retry(sensor, temp)
+
+    json_sensordata["temperature"] = "{0:0.1f}*C".format(t)
+    json_sensordata["humidity"] = "{1:0.1f}%".format(h)
+    
     sio.emit('order', json_sensordata)      
     print("send data")
     sio.emit('disconnect')
 
 sio.connect('http://localhost:3000')
+
+
