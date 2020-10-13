32a5bb4252e08c729813ef542cede6b7499b6ec4
JunHyeok Bae <bjh5098@naver.com>
2020-10-13 16:10:53 +0900

    edit PIR sensor reading part

--- raspberry/SensorControl/PIR_Sensor_datasend.py
+++ raspberry/SensorControl/PIR_Sensor_datasend.py
@@ -10,16 +10,17 @@ GPIO.setup(pir,GPIO.IN)
 GPIO.setup(led,GPIO.OUT)
 
 sio = socketio.Client()
+PIR_status = False
 
 json_sensordata = OrderedDict()
 
 def ledblink():
 
-    if GPIO.input(pir) == True: #센서 ON
+    if PIR_status == True: #센서 ON
         #print("Sensor ON")
         GPIO.output(led, GPIO.HIGH)
             
-    if GPIO.input(pir) == False: #센서 OFF
+    elif PIR_status == False: #센서 OFF
         #print("Sensor OFF")
         GPIO.output(led, GPIO.LOW)
 
@@ -28,12 +29,14 @@ def set_interval(func, sec):
     def func_wrapper():
         set_interval(func, sec)
         func()
+    PIR_status = GPIO.input(pir)
     t = threading.Timer(sec, func_wrapper)
     t.start()
     return t
 
 
-set_interval(ledblink,0.2) #0.2초 간격으로 Led Blink 함수 실행
+set_interval(ledblink,0.1) #0.2초 간격으로 Led Blink 함수 실행
+
 
 @sio.on('connect')
 def handler():
@@ -48,7 +51,7 @@ def handler(data):
     #if(data.value = "some order"){}
     #elif(data.value = "other order"){}
     print("recieved order")
-    json_sensordata["value"] = GPIO.input(pir)
+    json_sensordata["value"] = PIR_status
     sio.emit('order', json_sensordata)      
     print("send data")
     sio.emit('disconnect')
