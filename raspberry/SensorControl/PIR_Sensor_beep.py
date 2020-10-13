02ffcc96b693a042f33d09847603b598ef0d2023
JunHyeok Bae <bjh5098@naver.com>
2020-10-13 17:19:39 +0900

    add beep speaker

--- raspberry/SensorControl/PIR_Sensor_beep.py
+++ raspberry/SensorControl/PIR_Sensor_beep.py
@@ -3,12 +3,10 @@ import RPi.GPIO as GPIO
 import threading
 from collections import OrderedDict
 
-led = 18
 pir = 25
 beep = 12
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pir,GPIO.IN)
-GPIO.setup(led,GPIO.OUT)
 GPIO.setup(beep,GPIO.OUT)
 
 sio = socketio.Client()
@@ -16,15 +14,28 @@ PIR_status = GPIO.input(pir)
 
 json_sensordata = OrderedDict()
 
-def ledblink():
+def beep():
     PIR_status = GPIO.input(pir)
     if PIR_status == True: #센서 ON
         #print("Sensor ON")
-        GPIO.output(led, GPIO.HIGH)
+        scale = [ 261, 294, 329, 349, 392, 440, 493, 523 ]
+        list = [4, 4, 5, 5, 4, 4, 2, 4, 4, 2, 2, 1]
+        p = GPIO.PWM(beep, 100)
+        p.start(100)
+        p.ChangeDutyCycle(90)
+        for i in range(12):
+            p.ChangeFrequency(scale[list[i]])
+            if i == 6:
+                time.sleep(1)
+            else :
+                time.sleep(0.5)
+        p.stop()
+
+
             
     elif PIR_status == False: #센서 OFF
         #print("Sensor OFF")
-        GPIO.output(led, GPIO.LOW)
+        #GPIO.output(led, GPIO.LOW)
 
 
 def set_interval(func, sec):
@@ -36,7 +47,7 @@ def set_interval(func, sec):
     return t
 
 
-set_interval(ledblink,0.05) #0.2초 간격으로 Led Blink 함수 실행
+set_interval(beep,0.05) #0.05초 간격으로 beep 함수 실행
 
 
 @sio.on('connect')
